7640fa404cdb574b40d33df3c002fbaa88b976f6
Atze de Vries <atze.wiebe.de.vries@coop.no>
2025-05-22 15:06:05 +0200

    add unit tests to internal terraform package

--- internal/terraform/terraform_test.go
+++ internal/terraform/terraform_test.go
@@ -1,9 +1,12 @@
 package terraform
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/coopnorge/mage/internal/core"
 )
 
 func TestFindTerraformFolders(t *testing.T) {
@@ -26,7 +29,74 @@ func TestFindTerraformFolders(t *testing.T) {
 			t.Chdir(tt.workdir)
 			got, gotErr := FindTerraformProjects(".")
 			assert.NoError(t, gotErr)
-			assert.Equal(t, tt.want, got)
+			assert.ElementsMatch(t, tt.want, got)
+		})
+	}
+}
+
+func TestInitUpgradet(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		workdir string
+	}{
+		{
+			name:    "Terraform InitUpgrade upgrades versions within constraints",
+			workdir: "testdata/init-upgrade",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, cleanup, _ := core.MkdirTemp()
+			_ = os.CopyFS(dir, os.DirFS(tt.workdir))
+			t.Chdir(dir)
+			t.Cleanup(func() {
+				cleanup()
+			})
+
+			gotErr := InitUpgrade(".")
+			assert.NoError(t, gotErr)
+			// check for dirs and files
+			assert.FileExists(t, ".terraform.lock.hcl")
+			assert.DirExists(t, ".terraform")
+
+			lockfile, gotErr := os.ReadFile(".terraform.lock.hcl")
+			assert.NoError(t, gotErr)
+			assert.Contains(t, string(lockfile), "version     = \"3.7.2\"")
+		})
+	}
+}
+
+func TestLockProviders(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		workdir string
+		wantErr bool
+	}{
+		{
+			name:    "Terraform LockProviders target should succeed",
+			workdir: "testdata/providers-lock",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, cleanup, _ := core.MkdirTemp()
+			_ = os.CopyFS(dir, os.DirFS(tt.workdir))
+			t.Chdir(dir)
+			t.Cleanup(func() {
+				cleanup()
+			})
+
+			gotErr := ProviderLock(".")
+			assert.NoError(t, gotErr)
+
+			lockfile, gotErr := os.ReadFile(".terraform.lock.hcl")
+			assert.NoError(t, gotErr)
+			resultlockfile, gotErr := os.ReadFile("result.terraform.lock.hcl")
+			assert.NoError(t, gotErr)
+
+			assert.Equal(t, string(lockfile), string(resultlockfile))
 		})
 	}
 }
