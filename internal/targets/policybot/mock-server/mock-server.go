7f1e6a5c2e96a414145918f6eb6c8b5c373eab9c
Pravin Dahal <pravin.dahal@coop.no>
2025-11-18 20:32:20 +0100

    fix: Lint issues

--- internal/targets/policybot/mock-server/mock-server.go
+++ internal/targets/policybot/mock-server/mock-server.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/api/app", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/app", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("{}"))
 		if err != nil {
