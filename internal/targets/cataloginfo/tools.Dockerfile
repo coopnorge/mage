215283744cea090d728a53f63d99bdaab5ebae4a
Pravin Dahal <pravin.dahal@coop.no>
2025-11-24 17:01:20 +0100

    feat: Add catalog-info validator

---
+++ internal/targets/cataloginfo/tools.Dockerfile
@@ -0,0 +1,13 @@
+FROM node:25-alpine@sha256:26ded7f450a0ad37241d2ae97ea521a59cb551a1785c8a950f74b0a291ad3aea AS backstage-entity-validator-arm64
+
+ENV BACKSTAGE_ENTITY_VALIDATOR_VERSION=0.5.0
+
+RUN npm install -g npm && npm install --global @roadiehq/backstage-entity-validator@$BACKSTAGE_ENTITY_VALIDATOR_VERSION
+
+RUN addgroup -S toolgroup && adduser -S tooluser -G toolgroup
+
+USER tooluser
+
+ENTRYPOINT ["validate-entity"]
+
+FROM backstage-entity-validator-arm64 AS backstage-entity-validator-amd64
---
+++ targets/terraformmodule/cataloginfo.go
@@ -0,0 +1,24 @@
+package terraformmodule
+
+import (
+	"context"
+
+	catalogInfoTargets "github.com/coopnorge/mage/internal/targets/cataloginfo"
+	"github.com/magefile/mage/mg"
+)
+
+// CatalogInfo is the magefile namespace to group CatalogInfo commands
+type CatalogInfo mg.Namespace
+
+// Validate validates all terraform projects
+func (CatalogInfo) Validate(ctx context.Context) error {
+	mg.CtxDeps(ctx, mg.F(catalogInfoTargets.Validate))
+	return nil
+}
+
+// Changes returns the string true or false depending on the fact that
+// the current branch contains changes compared to the main branch.
+func (CatalogInfo) Changes(ctx context.Context) error {
+	mg.CtxDeps(ctx, catalogInfoTargets.HasChanges)
+	return nil
+}
