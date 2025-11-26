a64cda38c62d6dca633027b7c16ad9cd134a4500
Pravin Dahal <pravin.dahal@coop.no>
2025-11-26 12:46:43 +0100

    chore: Remove unnecessary import

--- internal/targets/policybot/testdata/layout/magefiles/magefile.go
+++ internal/targets/policybot/testdata/layout/magefiles/magefile.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	//mage:import terraformmodule
-	_ "github.com/coopnorge/mage/targets/terraformmodule"
 	//mage:import goapp
 	_ "github.com/coopnorge/mage/targets/goapp"
 )
