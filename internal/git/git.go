a1af5f7114a0f85e10d77c443c8f9355483ebb92
Atze de Vries <atze.wiebe.de.vries@coop.no>
2025-12-10 16:40:12 +0100

    fix typo in command
    Signed-off-by: Atze de Vries <atze.wiebe.de.vries@coop.no>

--- internal/git/git.go
+++ internal/git/git.go
@@ -124,7 +124,7 @@ func onMainBranch() (bool, error) {
 	if ok {
 		return githubRef == "refs/heads/main", nil
 	}
-	branch, err := sh.Output("git", "ref-parse", "--abbrev-ref", "HEAD")
+	branch, err := sh.Output("git", "rev-parse", "--abbrev-ref", "HEAD")
 	if err != nil {
 		return false, err
 	}
