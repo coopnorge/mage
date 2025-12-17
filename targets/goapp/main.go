1b942a8fd92a36152068d599e05d953f5111eefe
Atze de Vries <atze.wiebe.de.vries@coop.no>
2025-12-17 08:32:45 +0100

    add targets to golib and 'all' target
    Signed-off-by: Atze de Vries <atze.wiebe.de.vries@coop.no>

--- targets/goapp/main.go
+++ targets/goapp/main.go
@@ -81,7 +81,7 @@ func Build(ctx context.Context) error {
 //
 // For details see [Go.Validate], [Terraform.Validate] and [Docker.Validate].
 func Validate(ctx context.Context) error {
-	mg.CtxDeps(ctx, Go.Validate, Docker.Validate, Terraform.Validate, CatalogInfo.Validate, PolicyBotConfig.Validate)
+	mg.CtxDeps(ctx, Go.Validate, Docker.Validate, Terraform.Validate, CatalogInfo.Validate, PolicyBotConfig.Validate, Pallets.Validate)
 	return nil
 }
 
