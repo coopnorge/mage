3593acfc1223365a0f8ff6726523ba3d9b849cb1
Bendik August Nesbø <bendik.august.nesbo@coop.no>
2026-02-18 15:46:18 +0100

    feat: Tighten the golangci-lint version (#537)
    golangci-lint often introduces changes to linters in minor-versions, so
    we don't want to suddenly run newer versions of the linter, unless it
    has been updated in Mage.

---
+++ internal/devtool/golangci-lint_test.go
@@ -0,0 +1,119 @@
+package devtool
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-version"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGolangCILintVersionIsSameMajorAndMinor(t *testing.T) {
+	type args struct {
+		devtoolVer string
+		currentVer string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "exact match",
+			args: args{
+				devtoolVer: "2.10.1",
+				currentVer: "2.10.1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "same major and minor, different patch lower",
+			args: args{
+				devtoolVer: "2.10.1",
+				currentVer: "2.10.0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "same major and minor, different patch higher",
+			args: args{
+				devtoolVer: "2.10.0",
+				currentVer: "2.10.1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "different minor higher",
+			args: args{
+				devtoolVer: "2.9.0",
+				currentVer: "2.10.0",
+			},
+			wantErr: true,
+		},
+		{
+			name: "different minor lower",
+			args: args{
+				devtoolVer: "2.10.0",
+				currentVer: "2.9.0",
+			},
+			wantErr: true,
+		},
+		{
+			name: "different major higher",
+			args: args{
+				devtoolVer: "1.0.0",
+				currentVer: "2.0.0",
+			},
+			wantErr: true,
+		},
+		{
+			name: "different major lower",
+			args: args{
+				devtoolVer: "2.0.0",
+				currentVer: "1.0.0",
+			},
+			wantErr: true,
+		},
+		{
+			name: "different major lower, but minor higher",
+			args: args{
+				devtoolVer: "2.0.0",
+				currentVer: "1.1.0",
+			},
+			wantErr: true,
+		},
+		{
+			name: "same major and minor, patch significantly higher",
+			args: args{
+				devtoolVer: "1.55.0",
+				currentVer: "1.55.20",
+			},
+			wantErr: false,
+		},
+		{
+			name: "same major and minor, patch significantly lower",
+			args: args{
+				devtoolVer: "1.55.20",
+				currentVer: "1.55.0",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gl := GoLangCILint{}
+
+			dVer, err := version.NewVersion(tt.args.devtoolVer)
+			require.NoError(t, err)
+
+			cVer, err := version.NewVersion(tt.args.currentVer)
+			require.NoError(t, err)
+
+			err = gl.versionIsSameMajorAndMinor(dVer, cVer)
+			if tt.wantErr {
+				require.Error(t, err, "expected error but got nil")
+			} else {
+				require.NoError(t, err, "got unexpected error")
+			}
+		})
+	}
+}
