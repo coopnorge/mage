9dcb3f80344d37bfe4fa3359dc645da38f2a5649
Atze de Vries <atze.wiebe.de.vries@coop.no>
2026-09-09 09:27:50 +0200

    fix failing test in ci
    Signed-off-by: Atze de Vries <atze.wiebe.de.vries@coop.no>

--- internal/targets/cataloginfo/cataloginfo_test.go
+++ internal/targets/cataloginfo/cataloginfo_test.go
@@ -1,6 +1,9 @@
 package cataloginfo
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -12,6 +15,21 @@ import (
 )
 
 func TestCatalogInfoTargets(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		resp, err := json.Marshal([]map[string]string{{"slug": "engineering"}})
+		require.NoError(t, err)
+		_, err = w.Write(resp)
+		require.NoError(t, err)
+	}))
+	t.Cleanup(func() {
+		server.Close()
+	})
+
+	t.Setenv("GITHUB_TOKEN", "fake-token")
+	t.Setenv("GITHUB_REPOSITORY", "coopnorge/test")
+	t.Setenv("GITHUB_API_URL", server.URL)
+	t.Setenv("CI", "true")
 	tests := []struct {
 		name        string
 		testProject string
